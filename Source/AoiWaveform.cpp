6df357109f49975dd7b292b4799e936fddd970cc
Akiyuki Okayasu <akiyuki@tkrworks>
2017-04-11 11:37:19 +0900

    波形描画クラスAoiWaveformに初期化とファイル読み込みの関数を追加。

--- Source/AoiWaveform.cpp
+++ Source/AoiWaveform.cpp
@@ -9,6 +9,8 @@
 #include "AoiWaveform.hpp"
 
 AoiWaveform::AoiWaveform()
+:
+thumbnailCache(2)
 {
     
 }
@@ -18,3 +20,22 @@ AoiWaveform::~AoiWaveform()
     
 }
 //==============================================================================
+void AoiWaveform::init(int sourceSamplesPerThumbnailSample, AudioFormatManager& formatManagerToUse)
+{
+    thumbnail = std::make_unique<AudioThumbnail>(sourceSamplesPerThumbnailSample, formatManagerToUse, thumbnailCache);
+    thumbnail->addChangeListener(this);
+}
+
+void AoiWaveform::readFromFile(File& file)
+{
+    thumbnail->setSource(new FileInputSource(file));
+}
+
+
+//==============================================================================
+void AoiWaveform::changeListenerCallback (ChangeBroadcaster* source)
+{
+    if(source == thumbnail.get())
+    {
+    }
+}
--- Source/AoiWaveform.hpp
+++ Source/AoiWaveform.hpp
@@ -11,15 +11,22 @@
 
 #include "../JuceLibraryCode/JuceHeader.h"
 
-class AoiWaveform
+class AoiWaveform:
+private ChangeListener
 {
 public:
     AoiWaveform();
     ~AoiWaveform();
     //==============================================================================
+    void init(int sourceSamplesPerThumbnailSample, AudioFormatManager& formatManagerToUse);
+    void readFromFile(File& file);
+    
     
 private:
+    void changeListenerCallback (ChangeBroadcaster* source) override;
     //==============================================================================
+    AudioThumbnailCache thumbnailCache;
+    std::unique_ptr<AudioThumbnail> thumbnail;
 };
 
 #endif /* AoiWaveform_hpp */
