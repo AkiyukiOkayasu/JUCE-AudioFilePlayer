bf4746f308591e0dd43f9483569f0bc02de38d7e
Tatsuya Shiozawa <shio.tatsu99@gmail.com>
2017-11-24 00:48:03 +0900

    Fix space.

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -127,7 +127,7 @@ void MainContentComponent::openButtonClicked()
 {
     FileChooser chooser("Select a audio file to play...",
                         File::nonexistent,
-		                formatManager.getWildcardForAllFormats());
+                        formatManager.getWildcardForAllFormats());
     if(chooser.browseForFileToOpen())
     {
         File file(chooser.getResult());
